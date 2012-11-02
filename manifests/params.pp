6025db88ea17408620519405ae282a79e5cca8ca
Dave Simons <dave@inuits.eu>
2012-11-02 10:49:23 +0100

    add centos compat in default params
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,15 +2,18 @@ class apc::params {
 
   $pkg = $operatingsystem ? {
     /Debian|Ubuntu/ => 'php-apc',
+    CentOS          => 'php-pecl-apc',
   }
 
   #Note: apc does not macke much sense without php installed 
   $php = $operatingsystem ? {
     /Debian|Ubuntu/ => 'php5-cli',
+    CentOS          => 'php',
   } 
 
   $conf = $operatingsystem ? {
     /Debian|Ubuntu/ => '/etc/php5/apache2/conf.d/apc.ini/.anon/',
+    centOS          => '/etc/php.d/apc.ini/.anon/',
   }
 
   #specify the shared memory size
