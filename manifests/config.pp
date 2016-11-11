915b5a60dcd9792a28267e99b10a03c51a54529c
Pavel Pulec <kayn@inuits.eu>
2016-11-11 16:26:01 +0100

    fix php70 packages refs #25111
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,4 @@
+#
 class apc::config (
   $conf,
   $pkg,
@@ -18,7 +19,7 @@ class apc::config (
 
   if $apcu_backwards_compatibility {
     if $backwards_compatibility_pkg == undef {
-      warning("You did not specify the apcu backwards compatibility package name, it will not be installed!")
+      warning('You did not specify the apcu backwards compatibility package name, it will not be installed!')
     }
     else {
       package{ $backwards_compatibility_pkg:
@@ -34,43 +35,6 @@ class apc::config (
     require => [
       Package['apc']
     ],
-  }
-
-  # really ugly hack which will create symlink for package 'php70-php-pecl-apcu.x86_64'
-  # to the correct paths
-  if $pkg == 'php70-php-pecl-apcu.x86_64' {
-    file { '/usr/lib64/php/modules/apcu.so':
-      ensure  => 'link',
-      target  => '/opt/remi/php70/root/usr/lib64/php/modules/apcu.so',
-      require => Package['apc'],
-      notify  => Service['httpd'],
-    }
-
-    file { '/usr/lib64/php/modules/apc.so':
-      ensure  => 'link',
-      target  => '/opt/remi/php70/root/usr/lib64/php/modules/apc.so',
-      require => Package['apc'],
-      notify  => Service['httpd'],
-    }
-
-    file { '/var/lib/pear':
-      ensure => directory,
-      mode   => '0755',
-      owner  => 'root',
-      group  => 'root',
-    } ->
-    file { '/var/lib/pear/pkgxml':
-      ensure => directory,
-      mode   => '0755',
-      owner  => 'root',
-      group  => 'root',
-    } ->
-    file { '/var/lib/pear/pkgxml/php70-php-pecl-apcu.xml':
-      ensure  => 'link',
-      target  => '/opt/remi/php70/root/usr/lib64/php/modules/apcu.so',
-      require => Package['apc'],
-      notify  => Service['httpd'],
-    }
   }
 
 }
